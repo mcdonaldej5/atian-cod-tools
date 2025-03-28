1c82397c71ce62d04ec462632b333eff1f0c8e17
mcdonaldej5 <mcdonaldej5@gmail.com>
2025-03-27 21:41:26 -0500

    test v10

--- src/acts/includes.hpp
+++ src/acts/includes.hpp
@@ -13,16 +13,10 @@
 #include "mods/mods.hpp"
 #include "tools/tools.hpp"
 
-static auto info_response_hostname = ""s;
-static auto info_response_hostxuid = ""s;
 
 extern uintptr_t dwProcessBase;
 extern uintptr_t cgArray;
 extern uintptr_t cg_entitiesArray;
 
-extern float boldness;
-extern float flHue;
-extern float rgb_speed;
-
-const static auto Cbuf_AddText = reinterpret_cast<std::uintptr_t(__fastcall*)(int, const char*)>(qwProcessBase + 0x3CDE880);
-const static auto Cmd_ExecuteSingleCommand = reinterpret_cast<std::uintptr_t(__fastcall*)(int, int, const char* text, bool)>(qwProcessBase + 0x3CF4B80);
+const static auto Cbuf_AddText = reinterpret_cast<std::uintptr_t(__fastcall*)(int, const char*)>(dwProcessBase + 0x3CDE880);
+const static auto Cmd_ExecuteSingleCommand = reinterpret_cast<std::uintptr_t(__fastcall*)(int, int, const char* text, bool)>(dwProcessBase + 0x3CF4B80);
