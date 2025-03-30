df7d7c20fbd505ad41b8306f6ede8f763a159549
mcdonaldej5 <mcdonaldej5@gmail.com>
2025-03-30 15:07:04 -0500

    v4

--- src/shared/games/bo4/common.h
+++ src/shared/games/bo4/common.h
@@ -1,5 +1,9 @@
 #pragma once
 
+
+
+using namespace std;
+
 extern uintptr_t dwProcessBase;
 
 const static auto Cbuf_AddText = reinterpret_cast<std::uintptr_t(__fastcall*)(int, const char*)>(dwProcessBase + 0x3CDE880);
