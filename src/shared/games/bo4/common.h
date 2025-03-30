64de17474f4d90ffb3a7aadd1421537b6a79c835
mcdonaldej5 <mcdonaldej5@gmail.com>
2025-03-30 16:08:10 -0500

    test v10

--- src/shared/games/bo4/common.h
+++ src/shared/games/bo4/common.h
@@ -1,7 +1,9 @@
 #pragma once
 
 using namespace std;
-
+using namespace std::literals;
+using namespace std::string_literals;
+using namespace std::literals::string_literals;
 
 
 extern uintptr_t dwProcessBase;
