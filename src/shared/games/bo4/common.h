f00cb15d497a1001ed6ed226ebf1e7395c42e4df
mcdonaldej5 <mcdonaldej5@gmail.com>
2025-03-30 20:05:12 -0500

    t fix

--- src/shared/games/bo4/common.h
+++ src/shared/games/bo4/common.h
@@ -3,7 +3,18 @@
 #include <stdint.h>
 #include "includes.h"
 
+typedef INT8   int8_t;
+typedef INT16  int16_t;
+typedef INT32  int32_t;
+typedef INT64  int64_t;
+typedef UINT8  uint8_t;
+typedef UINT16 uint16_t;
+typedef UINT32 uint32_t;
+typedef UINT64 uint64_t;
 
+typedef unsigned __int64 __uint64_t;
+
+typedef __uint64_t bdUInt64;
 
 extern uintptr_t dwProcessBase;
 
