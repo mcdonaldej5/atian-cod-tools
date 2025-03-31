6712d7807c715678fdb60628d97a94116755a628
mcdonaldej5 <mcdonaldej5@gmail.com>
2025-03-30 22:27:57 -0500

    Revert "fix number 4"
    This reverts commit 94fbf07ae2117d945d73ed17fa0ab06b3dc78134.

--- src/acts/tools/bo4/lobby_tool.cpp
+++ src/acts/tools/bo4/lobby_tool.cpp
@@ -7,63 +7,9 @@
 #include <games/bo4/offsets.hpp>
 #include <utils/memapi_calls.hpp>
 
-using json = nlohmann::json;
-
-namespace io
-{
-	std::string get_json_file(const std::string & name);
-	std::ifstream read_json_file(const std::string & name);
-	json parse_json_file(const std::string& name);
-	bool write_file(const std::string & file, const std::string & data);
-	bool create_directory(const std::string & directory);
-	bool file_exists(const std::string & file);
-}
-
-namespace nt
-{
-	library::library()
-	{
-		this->module_ = GetModuleHandleA(nullptr);
-	}
-
-	PIMAGE_DOS_HEADER library::get_dos_header() const noexcept
-	{
-		return reinterpret_cast<PIMAGE_DOS_HEADER>(this->get_ptr());
-	}
-
-	std::uint8_t* library::get_ptr() const noexcept
-	{
-		return reinterpret_cast<std::uint8_t*>(this->module_);
-	}
-
-	bool library::is_valid() const noexcept
-	{
-		return this->module_ != nullptr && this->get_dos_header()->e_magic == IMAGE_DOS_SIGNATURE;
-	}
-
-	std::string library::get_path() const noexcept
-	{
-		if (!this->is_valid()) return "";
-
-		char name[MAX_PATH] = { 0 };
-		GetModuleFileNameA(this->module_, name, sizeof name);
-
-		return name;
-	}
-
-	std::string library::get_folder() const noexcept
-	{
-		if (!this->is_valid()) return "";
-
-		const auto path = std::filesystem::path(this->get_path());
-		return path.parent_path().generic_string();
-	}
-}
-
 inline __int64 __ROL8__(__int64 value, int count) { return __ROL__((__int64)value, count); }
 
 uintptr_t dwProcessBase = reinterpret_cast<uintptr_t>(GetModuleHandleA(NULL));
-
 
 namespace {
 	static const char* gametypes[]{
