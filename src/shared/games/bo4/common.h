aaf2b211571e91bbe7e79c71248ccbb39cc27333
mcdonaldej5 <mcdonaldej5@gmail.com>
2025-03-30 20:02:38 -0500

    tt

--- src/shared/games/bo4/common.h
+++ src/shared/games/bo4/common.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <cstdint>
+#include <stdint.h>
 #include "includes.h"
 
 
 
 extern uintptr_t dwProcessBase;
 
-const static auto Cbuf_AddText = reinterpret_cast<std::uintptr_t(__fastcall*)(const char*)>(dwProcessBase + 0x3CDE880);
+const static auto Cbuf_AddText = reinterpret_cast<std::uintptr_t(__fastcall*)(int, const char*)>(dwProcessBase + 0x3CDE880);
