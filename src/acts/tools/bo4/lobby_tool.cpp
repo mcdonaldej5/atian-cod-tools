47e9889a019563ddc50c2f3470f55b12f9faa771
mcdonaldej5 <mcdonaldej5@gmail.com>
2025-03-30 23:17:58 -0500

    22

--- src/acts/tools/bo4/lobby_tool.cpp
+++ src/acts/tools/bo4/lobby_tool.cpp
@@ -32,13 +32,12 @@ template<class T> T __ROL__(T value, int count)
 inline __int64 __ROL8__(__int64 value, int count) { return __ROL__((__int64)value, count); }
 
 uintptr_t dwProcessBase = reinterpret_cast<uintptr_t>(GetModuleHandleA(NULL));
-
+typedef void(*t_Cbuf_AddText)(int localClientNum, const char* text);
 void cbuf_addtext(const char* text) {
-    typedef void(*t_Cbuf_AddText)(int localClientNum, const char* text);
-    t_Cbuf_AddText Cbuf_AddText = reinterpret_cast<t_Cbuf_AddText>(0x3CDE880);
+    t_cbuf_addtext cbuf_addtext = reinterpret_cast<t_cbuf_addtext>(0x3CDE880);
 
-    if (Cbuf_AddText) {
-        Cbuf_AddText(0, text); // 0 is usually the local client index
+    if (cbuf_addtext) {
+        cbuf_addtext(0, text); // 0 is usually the local client index
     }
 }
 namespace {
@@ -510,7 +509,7 @@ namespace {
 		}
 		if (ImGui::Button("Launch Game"))
 		{
-			Cbuf_AddText(0, "launchgame");
+			cbuf_addtext(0, 'launchgame');
 		}
 
 		ImGui::SeparatorText("Blackout config");
